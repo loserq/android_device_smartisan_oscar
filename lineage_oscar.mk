170eb5a76e97f4647d6b618fd63b7ecef4130650
loserq <2650009628@qq.com>
2020-07-09 17:33:28 +0800

    oscar: lineage-ify

--- lineage_oscar.mk
+++ lineage_oscar.mk
@@ -23,12 +23,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/smartisan/oscar/device.mk)
 
 # Inherit some common MK stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.mk.maintainer=XiNGRZ
 
-PRODUCT_NAME := mk_oscar
+PRODUCT_NAME := lineage_oscar
 PRODUCT_BRAND := SMARTISAN
 PRODUCT_DEVICE := oscar
 PRODUCT_MANUFACTURER := smartisan
